0e03b2a3ff4078a47a5775470f3603fc2d90f6cb
niceandfun <niceokda@gmail.com>
2022-11-14 23:58:46 +0300

    Добавил метод обновления резюме

--- pages/resumes_page.py
+++ pages/resumes_page.py
@@ -1,12 +1,13 @@
 from selenium.webdriver.chrome import webdriver
+from selenium.webdriver.common.by import By
 
 from pages.page import Page
 
 
 class ResumesPage(Page):
-    def __init__(self, driver: webdriver):
+    def __init__(self, driver):
         super().__init__(driver=driver)
         self.url = 'https://hh.ru/applicant/resumes'
 
     def update_resume(self, title):
-        pass
+        self.driver.find_element(By.CSS_SELECTOR, f'[data-qa-title="{title}"] [data-qa="resume-update-button_actions"]')
